8943a8e56366509ee9032bcd3dd4faa3a1f0bc63
Roshan Cheereth <roshan.cmto@gmail.com>
2025-07-17 15:20:23 +0530

    Updated files

--- src/main/resources/demo-data.sql
+++ src/main/resources/demo-data.sql
@@ -7,7 +7,7 @@ VALUES ('The Time Traveller', 'English'),
 
 
 INSERT INTO show_ (theatre, city, show_date_time, movie_id)
-VALUES ('Galaxy Cinema',    'Bangalore', '2025-07-20T00:00', 1),
+VALUES ('Galaxy Cinema',    'Bangalore', '2025-07-21T00:00', 1),
        ('Galaxy Cinema',     'Bangalore',    '2025-07-20T00:00', 1),
        ('Prasads IMAX',     'Hyderabad', '2025-07-21T00:00', 2),
        ('Cinépolis Madrid', 'Madrid',    '2025-07-20T00:00', 3);
